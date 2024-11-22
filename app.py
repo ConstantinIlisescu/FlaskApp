d7404cc249d62053c88f282988c45b53dfddf912
Constantin Ilisescu <ilisescu@amazon.com>
2024-11-22 15:02:09 +0000

    Update a specific store

--- app.py
+++ app.py
@@ -23,4 +23,16 @@ def create_store():
     request_data = request.get_json()
     new_store = {"name":request_data["name"], "items":[]}
     stores.append(new_store)
+    print(stores)
     return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    print(stores)
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name":request_data["name"], "price":request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
