e17c6d2c585dc509c5aa1ed23c0bc00563ca962b
Constantin Ilisescu <ilisescu@amazon.com>
2024-11-22 13:49:32 +0000

    Add first flask endpoint

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
+
+
+store = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name" : "Chair",
+                "price" : 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store():
+    return{"store":store}
