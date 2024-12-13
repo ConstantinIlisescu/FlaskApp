24d8858c25a72b3ce6cc8802f909fab41f48adf7
Constantin Ilisescu <ilisescu@amazon.com>
2024-12-13 14:23:55 +0000

    General API improvements (validation)

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 
@@ -18,14 +19,24 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message":"Store not found"}, 404
+        abort(404, message="Store not found.")
     
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload." 
+        )
+    
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
     store_id = uuid.uuid4().hex
-    store = {**store_data, id: store_id}
+    store = {**store_data, "id": store_id}
     stores[store_id] = store
+    print(store)
     return store, 201
     
 
@@ -39,17 +50,33 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message":"Item not found"}, 404
-
+        abort(404, message="Item not found.")
 
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    # Here not only we need to validate data exists,
+    # but also what type of data. Price should be a float, etc.
+    if(
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. ensure 'price', 'store_id', and 'name' are included in the JSON payload"
+        )
+    for item in items.values():
+        if(
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found.")
     
     item_id = uuid.uuid4().hex
-    item = {**item_data, id: item_id}
+    item = {**item_data, "id": item_id}
     item[item_id] = item
     
     return item, 201
