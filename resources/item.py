cd5807fcc0f769a5a0765c2204181d35bfe1cc05
Constantin Ilisescu <ilisescu@amazon.com>
2024-12-20 15:38:21 +0000

    Implement the marshmellow schema

--- resources/item.py
+++ resources/item.py
@@ -2,8 +2,10 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+
 from db import items
 from db import stores
+from schemas import ItemSchema, ItemUpdateSchema
 
 
 blp = Blueprint("items", __name__, description="Operations on items")
@@ -25,12 +27,8 @@ class Item(MethodView):
         except KeyError:
             abort(404, message="Item not found.")
 
-
-    def put(self, item_id):
-        item_data = request.get_json()
-        if "price" not in item_data or "name" not in item_data:
-            abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
-
+    @blp.arguments(ItemUpdateSchema)
+    def put(self, item_data, item_id):
         try:
             item = items[item_id]
             item |= item_data  # syntax to update an dictionary, the items from the item_data will replace the values from item or add them to item
@@ -45,20 +43,8 @@ class ItemList(MethodView):
     def get(self):
         return{"items":list(items.values())}
             
-
-    def post(self):
-        item_data = request.get_json()
-        # Here not only we need to validate data exists,
-        # but also what type of data. Price should be a float, etc.
-        if(
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. ensure 'price', 'store_id', and 'name' are included in the JSON payload"
-            )
+    @blp.arguments(ItemSchema)
+    def post(self, item_data):
         for item in items.values():
             if(
                 item_data["name"] == item["name"]
